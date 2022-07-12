90946fe7a1bb66237a05b43268218309ef03a612
Vasari-Guest <vasari-guest@Vasari2iMacPro5.local>
2022-07-12 13:40:31 +0100

    update items

---
+++ VA_data/get_items.py
@@ -0,0 +1,13 @@
+import pickle 
+
+with open('saved_image_data_dict.pkl', 'rb') as f:
+    loaded_dict = pickle.load(f)
+
+categories = []
+
+for k,v in loaded_dict.items():
+    if len(loaded_dict[k]['artists']) == 0:
+        continue
+    if loaded_dict[k]['artists'][0] not in categories:
+        categories.append(loaded_dict[k]['artists'][0])
+print(categories)
